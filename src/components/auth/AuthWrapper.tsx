1aeffcd819925bea59efc0e051f328fba3227d88
Egor312-33 <186381521+Egor312-33@users.noreply.github.com>
2026-01-16 20:09:30 +0700

    create: ui component typography

--- src/components/auth/AuthWrapper.tsx
+++ src/components/auth/AuthWrapper.tsx
@@ -1,6 +1,7 @@
 
 import Link from 'next/link'
 import { type PropsWithChildren } from 'react'
+import Typography from '../ui/typography'
 
 interface AuthWrapperProps {
     heading: string
@@ -22,9 +23,9 @@ export function AuthWrapper({
     return (
         <div className="w-full max-w-105 mx-auto my-12 p-6 bg-linear-to-br from-secondary to-secondary-dark border-2 border-border rounded-2xl shadow-lg flex flex-col gap-6">
             <div>
-                <h3 className="text-3xl font-semibold text-primary mb-2">{heading}</h3>
+                <Typography variant='title-1' tag='h3'> {heading} </Typography>
                 {description && (
-                    <p className="text-sm text-muted-foreground leading-relaxed">{description}</p>
+                    <Typography variant='sub-title' tag='p'>{description}</Typography>
                 )}
             </div>
             <div className="flex flex-col gap-6">
---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,57 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "../lib/utils";
+
+
+type TypographyTag = 'h1' | 'h2' | 'h3' | 'h4' | 'span' | 'div' | 'p';
+
+
+const typographyVariants = cva("", {
+    variants: {
+        variant: {
+            "title-1": "text-3xl font-semibold text-primary mb-2",
+            "title-2": "scroll-m-20 text-3xl font-semibold tracking-tight",
+            "title-3": "scroll-m-20 text-2xl font-semibold tracking-tight",
+            "title-4": "scroll-m-20 text-xl font-semibold tracking-tight",
+            "title-5": "text-lg font-semibold",
+            "sub-title": "text-sm text-muted-foreground leading-relaxed",
+            "body-1": "text-base leading-7",
+            "body-2": "text-sm font-medium leading-none",
+            "body-3": "text-sm text-muted-foreground leading-relaxed",
+            "link": "font-medium text-primary underline underline-offset-4 hover:opacity-80 transition-opacity",
+        },
+    },
+    defaultVariants: {
+        variant: "body-1",
+    },
+});
+
+
+export type TypographyProps<T extends TypographyTag> =
+    React.ComponentPropsWithoutRef<T> &
+    VariantProps<typeof typographyVariants> & {
+        tag?: T;
+        children: React.ReactNode;
+    };
+
+
+export const Typography = <T extends TypographyTag = 'div'>({
+    children,
+    variant,
+    tag,
+    className,
+    ...props
+}: TypographyProps<T>) => {
+
+    const Component = tag || 'div';
+
+    return (
+        <Component
+            className={cn(typographyVariants({ variant }), className)}
+            {...props}
+        >
+            {children}
+        </Component>
+    );
+};
+
+export default Typography;
