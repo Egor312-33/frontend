7b8c36b06786b0a58e19b039bba19ad6f931d603
Egor312-33 <186381521+Egor312-33@users.noreply.github.com>
2026-01-21 16:16:49 +0700

    fix: app puth login/passwd

--- src/app/auth/login/passwd/page.tsx
+++ src/app/auth/login/passwd/page.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from 'next'
 
-import { LoginForm } from '@/components/auth/login'
 
 export const metadata: Metadata = {
     title: 'Войти'
 }
 
 export default function LoginPage() {
-    return <LoginForm />
+    return <div> </div>
 }
